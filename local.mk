2877ac2999965ad8d5b8f875de2e67d47257012f
threader <michael.heltne@gmail.com>
2020-06-23 05:39:25 +0100

    suni7: the-c64: local.mk override for kernel, headers and linaro libc

---
+++ local.mk
@@ -0,0 +1,4 @@
+LINUX_OVERRIDE_SRCDIR = /home/thread/dev/thec64/threader/C64Mini-FEL-boot/linux-3.4-redquark
+LINUX_HEADERS_OVERRIDE_SRCDIR = /home/thread/dev/thec64/threader/C64Mini-FEL-boot/linux-3.4-redquark/include
+#UBOOT_OVERRIDE_SRCDIR = /xperiadev/thec64/buildroot/output-0.0/build/uboot-2016.09.01
+GLIBC_OVERRIDE_SRCDIR = /home/thread/dev/thec64/buildroot/linaro/glibc
